973bb69b67458a899f22e52b38bb3e92aa1d0a21
Umang Mishra <mishraumang288@gmail.com>
2025-09-30 11:07:21 +0200

    Deploy: merge out exported site

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/StudyInAustriaGuide',
+  // basePath: '/StudyInAustriaGuide',
   eslint: {
     ignoreDuringBuilds: true,
   },
