0149fe75a6156e5caedc4ae8ab832fde4449fb88
Umang Mishra <mishraumang288@gmail.com>
2025-10-06 21:11:45 +0200

    Add Google Analytics tracking tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,16 @@ export default function RootLayout({
 
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6144787726207338"
           crossorigin="anonymous"></script>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-22BD3183LY"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-22BD3183LY');
+        </script>
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
