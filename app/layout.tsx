cba4dc4b122f803dc9ead0adb49c5d714fc9aa02
Umang Mishra <mishraumang288@gmail.com>
2025-10-07 21:56:29 +0200

    Add sitemap.xml for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,39 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
+// export const metadata: Metadata = {
+//   title: "Study in Austria - Complete Guide for International Students",
+//   description:
+//     "Comprehensive guide for prospective students coming to Austria. Find everything you need to know about applications, visas, accommodation, and student life.",
+//   generator: "v0.app",
+// };
 export const metadata: Metadata = {
-  title: "Study in Austria - Complete Guide for International Students",
+  title: "Study in Austria 🇦🇹 | Complete Guide for International Students",
   description:
-    "Comprehensive guide for prospective students coming to Austria. Find everything you need to know about applications, visas, accommodation, and student life.",
-  generator: "v0.app",
+    "Your ultimate guide to studying in Austria: applications, visas, universities, scholarships, and student life . Everything you need to start your journey.",
+  generator: "Next.js & v0.app",
+  keywords: [
+    "study in Austria",
+    "Austrian universities",
+    "international students",
+    "Austrian visa",
+    "Austrian scholarships",
+    "study abroad Europe",
+    "Residence permit process",
+    "Visa D Checklist",
+    "Indian Community in Austria",
+  ],
+  openGraph: {
+    title: "Study in Austria - Complete Guide for International Students",
+    description:
+      "Everything you need to study in Austria: applications, visas, accommodation, and student life.",
+    url: "https://studyinaustriaguide.onrender.com",
+    siteName: "Study in Austria",
+    locale: "en_US",
+    type: "website",
+  },
 };
+
 
 export default function RootLayout({
   children,
@@ -19,21 +46,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-22BD3183LY"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-22BD3183LY');
-    `,
-          }}
-        />
 
         {/* Google Analytics */}
         <script
